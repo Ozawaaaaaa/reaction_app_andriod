1c8387efdc396a22893098ad7498f47c2a300239
Ming Shen <mshen2@wpi.edu>
2017-12-07 20:39:30 -0500

    sound color v2

--- app/src/main/java/com/sxz/reaction/ColorModeFragment.java
+++ app/src/main/java/com/sxz/reaction/ColorModeFragment.java
@@ -1,12 +1,15 @@
 package com.sxz.reaction;
 
+//import android.graphics.Color;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +18,6 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
 public class ColorModeFragment extends Fragment{
 
@@ -24,7 +26,6 @@ public class ColorModeFragment extends Fragment{
     private ConstraintLayout mBackground;
     private ArrayList<String> colors = new ArrayList<>();
     private int current_color = 0;
-    private boolean isStarted = false;
     private long start_time;
     private long end_time;
 
@@ -34,49 +35,75 @@ public class ColorModeFragment extends Fragment{
 
         View view = inflater.inflate(R.layout.fragment_color_mode, container, false);
 
-        colors.add("#E0E0E0");
-        colors.add("#A1887F");
+        colors.add("#EF5350"); // red
+        colors.add("#FFF176"); // yellow
+        colors.add("#C6FF00"); // green
 
         mStartColorChange = view.findViewById(R.id.start_color_change);
         mBackground = view.findViewById(R.id.color_mode_background);
         mQuit = view.findViewById(R.id.quit_to_choose_mode);
 
+        mBackground.setBackgroundColor(Color.parseColor(colors.get(0)));
 
         mStartColorChange.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 if (mStartColorChange.getText().equals("Start")) {
+                    mBackground.setBackgroundColor(Color.parseColor(colors.get(1)));
+
+                    current_color = 1; // now yellow
 
                     mStartColorChange.setText("Restart");
+
+                    mStartColorChange.setVisibility(View.GONE);
+                    mQuit.setVisibility(View.GONE);
+
+                    Random random = new Random(System.currentTimeMillis());
+                    int n = random.nextInt(5) + 1;
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            mBackground.setBackgroundColor(Color.parseColor(colors.get(2)));
+                            current_color = 2;
+
+                            start_time = System.nanoTime();
+                        }
+                    }, n*1000);
+
+                } else {
+
+                    Fragment fragment = new ColorModeFragment();
+                    FragmentManager fragmentManager = getFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.fragment_container, fragment);
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
                 }
-
-                Random random = new Random(System.currentTimeMillis());
-                int n = random.nextInt(5) + 1;
-                try {
-                    TimeUnit.SECONDS.sleep(n);
-                } catch (InterruptedException e) {}
-
-
-                current_color = (current_color + 1)%2;
-                isStarted = true;
-                mBackground.setBackgroundColor(Color.parseColor(colors.get(current_color)));
-                start_time = System.nanoTime();
-
             }
-
         });
+
 
         mBackground.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                if (isStarted) {
+                if (current_color == 2) {
                     end_time = System.nanoTime();
                     double time_difference = (end_time - start_time)/1e6;
-
-                    isStarted = false;
                     Toast.makeText(getContext(), Double.toString(time_difference), Toast.LENGTH_SHORT).show();
+
+                    mStartColorChange.setVisibility(View.VISIBLE);
+                    mQuit.setVisibility(View.VISIBLE);
+
+                } else {
+                    Toast.makeText(getContext(), "Color not changed yet", Toast.LENGTH_SHORT).show();
+
+                    Fragment fragment = new ColorModeFragment();
+                    FragmentManager fragmentManager = getFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.fragment_container, fragment);
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
                 }
             }
         });
--- app/src/main/java/com/sxz/reaction/SoundModeFragment.java
+++ app/src/main/java/com/sxz/reaction/SoundModeFragment.java
@@ -1,7 +1,9 @@
 package com.sxz.reaction;
 
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
@@ -14,7 +16,6 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
 public class SoundModeFragment extends Fragment {
 
@@ -23,9 +24,10 @@ public class SoundModeFragment extends Fragment {
     private ConstraintLayout mBackground;
     private MediaPlayer sound_high;
     private MediaPlayer sound_low;
-    private boolean isStarted = false;
     private long start_time;
     private long end_time;
+    private int current_played = 0;
+    private boolean rule_broken = false;
 
 
     @Nullable
@@ -41,51 +43,89 @@ public class SoundModeFragment extends Fragment {
         mQuit = view.findViewById(R.id.quit_to_choose_mode_from_sound_mode);
         mBackground = view.findViewById(R.id.sound_mode_background);
 
+
+
         mStartSound.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
-//                sound_low = MediaPlayer.create(getActivity().getApplicationContext(), R.raw.low);
-
                 if (mStartSound.getText().equals("Start")) {
 
                     mStartSound.setText("Restart");
+                    sound_high.start();
+                    mStartSound.setVisibility(View.GONE);
+                    mQuit.setVisibility(View.GONE);
+
+                    current_played = 1;
+
+                    Random random = new Random(System.currentTimeMillis());
+                    int n = random.nextInt(5) + 1;
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            if (!rule_broken) {
+                                sound_high.stop();
+                                sound_high = MediaPlayer.create(getActivity().getApplicationContext(), R.raw.high);
+                                sound_low.start();
+                                current_played = 2;
+                                start_time = System.nanoTime();
+                            }
+
+                        }
+                    }, n*1000);
+
+                } else { // if restart
+
+                    Fragment fragment = new SoundModeFragment();
+                    FragmentManager fragmentManager = getFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.fragment_container, fragment);
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
                 }
-
-                sound_high.start();
-
-                Random random = new Random(System.currentTimeMillis());
-                int n = random.nextInt(5) + 1;
-                try {
-                    TimeUnit.SECONDS.sleep(n);
-                } catch (InterruptedException e) {}
-
-                sound_high.stop();
-                sound_high = MediaPlayer.create(getActivity().getApplicationContext(), R.raw.high);
-
-                sound_low.start();
-                isStarted = true;
-                start_time = System.nanoTime();
-
             }
         });
 
 
         mBackground.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
-                if (isStarted) {
+                if (current_played == 2) {
+                    mStartSound.setVisibility(View.VISIBLE);
+                    mQuit.setVisibility(View.VISIBLE);
+
                     end_time = System.nanoTime();
                     double time_difference = (end_time - start_time)/1e6;
-
-                    isStarted = false;
                     Toast.makeText(getContext(), Double.toString(time_difference), Toast.LENGTH_SHORT).show();
 
                     sound_low.stop();
+                    current_played = 0;
                     sound_low = MediaPlayer.create(getActivity().getApplicationContext(), R.raw.low);
-                }
+
+                } else if (current_played == 1) {
+
+                    mStartSound.setVisibility(View.VISIBLE);
+                    mQuit.setVisibility(View.VISIBLE);
+
+                    Toast.makeText(getContext(), "Sound not changed yet", Toast.LENGTH_SHORT).show();
+//                    sound_high.stop();
+//                    sound_high = MediaPlayer.create(getActivity().getApplicationContext(), R.raw.high);
+//                    current_played = 0;
+//                    mStartSound.setText("Start");
+//                    rule_broken = true;
+
+                    sound_low.stop();
+                    sound_high.stop();
+
+                    Fragment fragment = new SoundModeFragment();
+                    FragmentManager fragmentManager = getFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.fragment_container, fragment);
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
+
+
+                } else {} // current nothing playing
             }
         });
 
@@ -98,6 +138,9 @@ public class SoundModeFragment extends Fragment {
                 fragmentTransaction.replace(R.id.fragment_container, fragment);
                 fragmentTransaction.addToBackStack(null);
                 fragmentTransaction.commit();
+
+                sound_high.stop();
+                sound_low.stop();
             }
         });
 
